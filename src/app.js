43dfd62a347ab960ab760d28c155ed9f029bd2d1
fedotovps <fedotovps@bk.ru>
2024-07-31 11:13:09 +0300

    Создание сервера на Express, подготовка основных CRUD операций, установка body-parser и cors

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRouter = require("./routes/users");
+
+// Вызываем функцию конфигурации
+dotenv.config();
+
+const app = express();
+
+// Адрес сервера и порт
+const { PORT = 3000, API_URL = "http://127.0.0.1" } = process.env;
+
+// Функция обработки данных
+const helloWorld = (request, response) => {
+  response.status(200);
+  response.send("Hello, World");
+};
+
+// с помощью app.use используем роутер и body-parser (помогает обрабатывать данные, которые приходят в теле сообщения)
+app.use(cors());
+app.use(bodyParser.json());
+app.use(userRouter);
+
+// Обработка get-запроса
+app.get("/", helloWorld);
+
+// Обработка post-pапроса
+app.post("/", (request, response) => {
+  response.status(200);
+  response.send("Hello from post");
+});
+
+// Запускаем сервер
+app.listen(PORT, () => {
+  console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`);
+});
