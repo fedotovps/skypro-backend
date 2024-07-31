82113b0c0d3ab19508dca084eef13de0e8d3e09f
fedotovps <fedotovps@bk.ru>
2024-07-31 11:25:04 +0300

    Подключение к MongoDb

--- src/app.js
+++ src/app.js
@@ -3,14 +3,27 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRouter = require("./routes/users");
+const mongoose = require("mongoose");
 
 // Вызываем функцию конфигурации
 dotenv.config();
 
-const app = express();
-
 // Адрес сервера и порт
-const { PORT = 3000, API_URL = "http://127.0.0.1" } = process.env;
+const {
+  PORT = 3000,
+  API_URL = "http://127.0.0.1",
+  MONGO_URL = "mongodb://localhost:27017/skyproBackend",
+} = process.env;
+
+// Подключение к БД
+try {
+  mongoose.connect(MONGO_URL);
+  console.log("Success connected to MongoDb");
+} catch (error) {
+  console.log(error);
+}
+
+const app = express();
 
 // Функция обработки данных
 const helloWorld = (request, response) => {
